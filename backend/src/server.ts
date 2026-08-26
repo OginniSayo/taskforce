749b03425eb76a9dbe9c051f87700def202beebc
Dralen <oginnisayofunmi12@gmail.com>
2026-08-26 01:02:01 +0100

    prepared for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,12 +5,22 @@ import connectDB from './config/db';
 import userRouter from './routes/userRoute';
 import taskRouter from './routes/taskRoute';
 
+import path from 'path'
+
 const app = express()
 
 const port = process.env.PORT || 3000;
 
 // middleware
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173", // allow requests from this origin (the frontend) "http://localhost:5173"
+      credentials: true
+    })
+  );
+} // enables CORS for all routes
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -21,11 +31,17 @@ connectDB()
 app.use('/api/user', userRouter);
 app.use('/api/tasks', taskRouter)
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('API working!')
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+}
+
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+    
+  })
 })
